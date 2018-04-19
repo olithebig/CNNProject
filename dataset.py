a55a1c1a02a39d236e16b0434e042266df315217
Marius Hirt <marius.hirt@hs-furtwangen>
2018-04-19 18:00:01 +0200

    updated .gitignore and slight reformatting

--- dataset.py
+++ dataset.py
@@ -12,14 +12,14 @@ def load_train(train_path, image_size, classes):
     cls = []
 
     print('Going to read training images')
-    for fields in classes:   
+    for fields in classes:
         index = classes.index(fields)
         print('Now going to read {} files (Index: {})'.format(fields, index))
         path = os.path.join(train_path, fields, '*g')
         files = glob.glob(path)
         for fl in files:
             image = cv2.imread(fl)
-            image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
+            image = cv2.resize(image, (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
             image = image.astype(np.float32)
             image = np.multiply(image, 1.0 / 255.0)
             images.append(image)
@@ -39,80 +39,79 @@ def load_train(train_path, image_size, classes):
 
 class DataSet(object):
 
-  def __init__(self, images, labels, img_names, cls):
-    self._num_examples = images.shape[0]
+    def __init__(self, images, labels, img_names, cls):
+        self._num_examples = images.shape[0]
 
-    self._images = images
-    self._labels = labels
-    self._img_names = img_names
-    self._cls = cls
-    self._epochs_done = 0
-    self._index_in_epoch = 0
+        self._images = images
+        self._labels = labels
+        self._img_names = img_names
+        self._cls = cls
+        self._epochs_done = 0
+        self._index_in_epoch = 0
 
-  @property
-  def images(self):
-    return self._images
+    @property
+    def images(self):
+        return self._images
 
-  @property
-  def labels(self):
-    return self._labels
+    @property
+    def labels(self):
+        return self._labels
 
-  @property
-  def img_names(self):
-    return self._img_names
+    @property
+    def img_names(self):
+        return self._img_names
 
-  @property
-  def cls(self):
-    return self._cls
+    @property
+    def cls(self):
+        return self._cls
 
-  @property
-  def num_examples(self):
-    return self._num_examples
+    @property
+    def num_examples(self):
+        return self._num_examples
 
-  @property
-  def epochs_done(self):
-    return self._epochs_done
+    @property
+    def epochs_done(self):
+        return self._epochs_done
 
-  def next_batch(self, batch_size):
-    """Return the next `batch_size` examples from this data set."""
-    start = self._index_in_epoch
-    self._index_in_epoch += batch_size
+    def next_batch(self, batch_size):
+        """Return the next `batch_size` examples from this data set."""
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
 
-    if self._index_in_epoch > self._num_examples:
-      # After each epoch we update this
-      self._epochs_done += 1
-      start = 0
-      self._index_in_epoch = batch_size
-      assert batch_size <= self._num_examples
-    end = self._index_in_epoch
+        if self._index_in_epoch > self._num_examples:
+            # After each epoch we update this
+            self._epochs_done += 1
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
 
-    return self._images[start:end], self._labels[start:end], self._img_names[start:end], self._cls[start:end]
+        return self._images[start:end], self._labels[start:end], self._img_names[start:end], self._cls[start:end]
 
 
 def read_train_sets(train_path, image_size, classes, validation_size):
-  class DataSets(object):
-    pass
-  data_sets = DataSets()
+    class DataSets(object):
+        pass
 
-  images, labels, img_names, cls = load_train(train_path, image_size, classes)
-  images, labels, img_names, cls = shuffle(images, labels, img_names, cls)  
+    data_sets = DataSets()
 
-  if isinstance(validation_size, float):
-    validation_size = int(validation_size * images.shape[0])
+    images, labels, img_names, cls = load_train(train_path, image_size, classes)
+    images, labels, img_names, cls = shuffle(images, labels, img_names, cls)
 
-  validation_images = images[:validation_size]
-  validation_labels = labels[:validation_size]
-  validation_img_names = img_names[:validation_size]
-  validation_cls = cls[:validation_size]
+    if isinstance(validation_size, float):
+        validation_size = int(validation_size * images.shape[0])
 
-  train_images = images[validation_size:]
-  train_labels = labels[validation_size:]
-  train_img_names = img_names[validation_size:]
-  train_cls = cls[validation_size:]
+    validation_images = images[:validation_size]
+    validation_labels = labels[:validation_size]
+    validation_img_names = img_names[:validation_size]
+    validation_cls = cls[:validation_size]
 
-  data_sets.train = DataSet(train_images, train_labels, train_img_names, train_cls)
-  data_sets.valid = DataSet(validation_images, validation_labels, validation_img_names, validation_cls)
+    train_images = images[validation_size:]
+    train_labels = labels[validation_size:]
+    train_img_names = img_names[validation_size:]
+    train_cls = cls[validation_size:]
 
-  return data_sets
+    data_sets.train = DataSet(train_images, train_labels, train_img_names, train_cls)
+    data_sets.valid = DataSet(validation_images, validation_labels, validation_img_names, validation_cls)
 
-
+    return data_sets
