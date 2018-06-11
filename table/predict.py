8979ad0b90d52399e07e37cd7d2ce363d87a1ba7
OliverMattes <jeova_sanctus_unus@gmx.de>
2018-06-11 15:05:20 +0200

    modified Predict scripts to return their result in a usable form

--- table/predict.py
+++ table/predict.py
@@ -1,58 +1,64 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+def predict():
+	# Image file of the glass
+	filename = 'training_data/dirty/0.jpg'
 
-# Image file of the glass
-filename = 'test_data/occupied/176.jpg'
 
+	image_size = 128
+	num_channels = 3
+	images = []
+	# Reading the image using OpenCV
+	image = cv2.imread(filename)
+	# Resizing the image to our desired size and pre-processing will be done exactly as done during training
+	image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
+	images.append(image)
+	images = np.array(images, dtype=np.uint8)
+	images = images.astype('float32')
+	images = np.multiply(images, 1.0 / 255.0)
+	# The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
+	x_batch = images.reshape(1, image_size, image_size, num_channels)
 
-image_size = 128
-num_channels = 3
-images = []
-# Reading the image using OpenCV
-image = cv2.imread(filename)
-# Resizing the image to our desired size and pre-processing will be done exactly as done during training
-image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
-images.append(image)
-images = np.array(images, dtype=np.uint8)
-images = images.astype('float32')
-images = np.multiply(images, 1.0 / 255.0)
-# The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
-x_batch = images.reshape(1, image_size, image_size, num_channels)
+	# Let us restore the saved model
+	sess = tf.Session()
+	# Step-1: Recreate the network graph. At this step only graph is created.
+	saver = tf.train.import_meta_graph('table-model.meta')
+	# Step-2: Now let's load the weights saved using the restore method.
+	saver.restore(sess, tf.train.latest_checkpoint('./'))
 
-# Let us restore the saved model
-sess = tf.Session()
-# Step-1: Recreate the network graph. At this step only graph is created.
-saver = tf.train.import_meta_graph('table-model.meta')
-# Step-2: Now let's load the weights saved using the restore method.
-saver.restore(sess, tf.train.latest_checkpoint('./'))
+	# Accessing the default graph which we have restored
+	graph = tf.get_default_graph()
+	file_writer = tf.summary.FileWriter('', sess.graph)
 
-# Accessing the default graph which we have restored
-graph = tf.get_default_graph()
-file_writer = tf.summary.FileWriter('', sess.graph)
+	# Now, let's get hold of the op that we can be processed to get the output.
+	# In the original network y_pred is the tensor that is the prediction of the network
+	y_pred = graph.get_tensor_by_name("y_pred:0")
 
-# Now, let's get hold of the op that we can be processed to get the output.
-# In the original network y_pred is the tensor that is the prediction of the network
-y_pred = graph.get_tensor_by_name("y_pred:0")
+	# Let's feed the images to the input placeholders
+	x = graph.get_tensor_by_name("x:0")
+	y_true = graph.get_tensor_by_name("y_true:0")
+	y_test_images = np.zeros((1, 3))
 
-# Let's feed the images to the input placeholders
-x = graph.get_tensor_by_name("x:0")
-y_true = graph.get_tensor_by_name("y_true:0")
-y_test_images = np.zeros((1, 3))
+	image_shaped_input = tf.reshape(x, [1, 128, 128, 3])
+	tf.summary.image('input', image_shaped_input, 10)
 
-image_shaped_input = tf.reshape(x, [1, 128, 128, 3])
-tf.summary.image('input', image_shaped_input, 10)
+	# Creating the feed_dict that is required to be fed to calculate y_pred
+	feed_dict_testing = {x: x_batch, y_true: y_test_images}
+	result = sess.run(y_pred, feed_dict=feed_dict_testing)
 
-# Creating the feed_dict that is required to be fed to calculate y_pred
-feed_dict_testing = {x: x_batch, y_true: y_test_images}
-result = sess.run(y_pred, feed_dict=feed_dict_testing)
+	merged = tf.summary.merge_all()
 
-merged = tf.summary.merge_all()
+	# Results
+	classes = ['clean', 'dirty', 'occupied']
+	index = 0
+	propability = 0
+	for i in range(classes.__len__()):
+		if ( result[0][i]>propability ):
+			propability = result[0][i]
+			index = i
 
-# Results
+	return classes[index]
 
-classes = ['clean', 'dirty', 'occupied']
+print (predict())
 
-for i in range(classes.__len__()):
-    print(classes[i])
-    print(result[0][i])
