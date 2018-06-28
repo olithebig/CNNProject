c04523df4128133df9ce7fb196eed8724ffcb795
OliverMattes <jeova_sanctus_unus@gmx.de>
2018-06-28 15:14:32 +0200

    changed the tables/crop_images.py to be the correct version... hopefully

--- table/crop_images.py
+++ table/crop_images.py
@@ -7,7 +7,7 @@ images = glob.glob("*.jpg")
 # Crops all images in directory, where it is executed
 for image in images:
     img = Image.open(image)
-    area = (1050, 0, 1920, 1080)
+    area = (300, 0, 880, 720)
     cropped_img = img.crop(area)
     filename = repr(i) + ".jpg"
     cropped_img.save(filename)
